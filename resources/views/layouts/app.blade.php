3a9c26b208fd22519b62f73a9e552acdf52dbbd5
ElodieBou44 <elodieboulanger4@outlook.com>
2023-10-26 14:58:07 -0400

    Structuration des dossiers css et assets dans le dossier public

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     <title>Vino</title>
 </head>
 <body>
@@ -13,7 +13,7 @@
             <li class="main-nav-item">        
                 <a href="#">
                     <figure class="container-icons-navbar active">
-                        <img src="" alt="Accueil">
+                        <img src="{{ asset('assets/icons/home_icon.svg') }}" alt="Accueil">
                         <figcaption>accueil</figcaption>
                     </figure>
                 </a>
@@ -21,15 +21,15 @@
             <li class="main-nav-item">        
                 <a href="#">
                     <figure class="container-icons-navbar">
-                        <img src="" alt="Recherche">
-                        <figcaption>recherche</figcaption>
+                        <img src="{{ asset('assets/icons/search_icon.svg') }}" alt="Recherche">
+                        <figcaption class="icons-label">recherche</figcaption>
                     </figure>
                 </a>
             </li>
             <li class="main-nav-item">
                 <a href="#">
                     <figure class="container-icons-navbar">
-                        <img src="" alt="Liste d'achats">
+                        <img src="{{ asset('assets/icons/list_icon.svg') }}" alt="Liste d'achats">
                         <figcaption>liste</figcaption>
                     </figure>
                 </a>
@@ -37,7 +37,7 @@
             <li class="main-nav-item">        
                 <a href="#">
                     <figure class="container-icons-navbar">
-                        <img src="" alt="Celliers">
+                        <img src="{{ asset('assets/icons/cellars_icon.svg') }}" alt="Celliers">
                         <figcaption>celliers</figcaption>
                     </figure>
                 </a>
@@ -45,7 +45,7 @@
             <li class="main-nav-item">         
                 <a href="#">
                     <figure class="container-icons-navbar">
-                        <img src="" alt="Profil">
+                        <img src="{{ asset('assets/icons/profile_icon.svg') }}" alt="Profil">
                         <figcaption>profil</figcaption>
                     </figure>
                 </a>
