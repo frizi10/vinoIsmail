841dfc6fe45d112c1e97b3ff686efdc9a8689a36
ElodieBou44 <elodieboulanger4@outlook.com>
2023-11-03 11:24:17 -0400

    Ajout de DSstore dans gitignore + Suppression des anciennes routes commentées

--- routes/web.php
+++ routes/web.php
@@ -101,29 +101,6 @@ Route::middleware(['auth'])->group(function () {
     // Modification de la quantité de bouteilles se trouvant dans une même liste
     Route::put('/listes/{liste_id}/bouteilles/{bouteille_id}', [BouteilleController::class, 'modifierQuantiteListe'])->name('bouteilles.modifierQuantiteListe');
 
-    // Anciennes routes de gestion des bouteilles 
-    // Route::get('/bouteilles/search', [BouteilleController::class, 'search'])->name('bouteilles.search');
-    // Route::get('/bouteilles/addBouteilleSearch/{id}', [BouteilleController::class, 'addBouteilleSearch'])->name('bouteilles.addBouteilleSearch');
-
-    // Anciennes routes des bouteilles d'un cellier
-    // Route::get('/Ajouter-bouteilles/{cellier_id}', [BouteilleController::class, 'index'])->name('Ajouter-bouteilles');
-    // Route::get('/Ajouter-bouteille-manuellement/{cellier_id}', [BouteilleController::class, 'AjouterbouteilleManuellement'])->name('Ajouter-bouteille-manuellement');
-    // Route::post('/addBouteilleManuellementPost', [BouteilleController::class, 'addBouteilleManuellementPost'])->name('addBouteilleManuellementPost');
-    // Route::post('/bouteilles/addBouteille/{id}', [BouteilleController::class, 'addBouteille'])->name('bouteilles.addBouteille');
-    // Route::delete('/bouteilles/{id}', [BouteilleController::class, 'destroy'])->name('bouteilles.destroy');
-    // Route::get('/mon-cellier', [CellierController::class, 'index'])->name('mon-cellier');
-    // Route::get('/celliers/{cellier_id}', [BouteilleCellierController::class, 'index'])->name('celliers.show'); 
-
-    // Pour modifier la quantité d'une bouteille dans un cellier
-    // Route::get('/modifierQte/{bouteille_id}', [BouteilleController::class, 'modifierBouteille'])->name('modifier-Qte');
-    // Route::post('/modifierQte/{bouteille_id}', [BouteilleController::class, 'modifierQteBouteille'])->name('modifier-Qte');
-
-    // Recherche de bouteilles pour le footer
-    // Route::get('/bouteilles/rechercheFooterBouteille', [BouteilleController::class, 'rechercheFooterBouteille'])->name('bouteilles.rechercheFooterBouteille');
-    // Route::post('/bouteilles/rechercheFooterBouteillePost', [BouteilleController::class, 'indexRecherche'])->name('bouteilles.rechercheFooterBouteillePost');
-
-    // Redirection vers HomeController après authentification
-    // Route::get('/home', [HomeController::class, 'index'])->name('home');
 });
 
 
