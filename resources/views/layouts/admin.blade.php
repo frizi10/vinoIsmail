fb13df07018113277cff815ba1789a3c813b30c5
ElodieBou44 <elodieboulanger4@outlook.com>
2023-11-06 19:23:49 -0500

    Tests de CSS/HTML pour les pages admin à partir du layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,6 +7,82 @@
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://use.typekit.net/dox8qou.css">
     <title>Vino - @yield('title')</title>
+    <style>
+
+        .admin-nav-list {
+            width: 100%;
+            display: grid;
+            grid-template-columns: repeat(4, minmax(20%, auto));
+            position: fixed;
+            bottom: 0;
+            padding-bottom: 0.5em;
+            background-color: white;
+        }
+
+        .admin-table-container {
+            width: 100%; 
+            padding: 1rem 1rem;
+        }
+
+        .admin-show-container {
+            width: 100%; 
+        }
+
+        /* .admin-table-container-show {
+            display: flex;
+            flex-wrap: wrap; 
+            border: 1px solid #000000; 
+            border-radius: 10px; 
+            margin: .5rem;
+        }
+
+        .table-row{
+            width: 100%;
+            display: flex;
+            align-items: center;
+            border-bottom: 1px solid #000000;
+        }
+
+        .table-header {
+            width: 8rem;
+            text-align: left; 
+            margin: 1rem;
+        }
+
+        .table-data{
+            max-width: ;
+        } */
+        .admin-table-container-show {
+            display: flex;
+            flex-direction: column;
+            border: 1px solid #000000;
+            border-radius: 10px;
+            margin: .5rem;
+        }
+
+        .table-row {
+            display: flex;
+            align-items: center;
+        }
+
+        .table-row:not(:last-child) {
+            border-bottom: 1px solid #000000;
+        }
+
+        .table-header {
+            width: 33.33%; /* 1/3 de la largeur */
+            text-align: left;
+            margin: 1rem;
+        }
+
+        .table-data {
+            width: 66.66%; /* 2/3 de la largeur */
+            max-width: 100%;
+            word-break: break-all;
+            margin: 1rem;
+        }
+
+    </style>
 </head>
 <body>
     @if(session('success'))
@@ -25,7 +101,7 @@
     @endif
     @yield('content')
     <nav class="main-nav">
-        <ul class="main-nav-list">
+        <ul class="admin-nav-list">
             <li class="main-nav-item">        
                 <a href="#">
                     <figure class="container-icons-navbar active">
