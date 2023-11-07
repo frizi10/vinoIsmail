69492b964c2b2b18a293a54349cbe61e666da0b8
ElodieBou44 <elodieboulanger4@outlook.com>
2023-11-07 14:20:39 -0500

    Amélioration du formulaire de création d'un utilisateur par un administrateur

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -1,39 +1,6 @@
 @extends('layouts.app')
 @section('title', 'Bienvenue')
 @section('content')
-<!-- <div class="container">
-    <div class="welcome">
-        <h2 class="welcome-title">Bienvenue chez <span class="welcome-vino">vino</span>!</h2>
-        <p class="welcome-text">L'outil le plus simple et efficace pour gérer vos celliers et vos achats SAQ.</p>
-    </div>
-    <img src="{{ asset('assets/img/img_connexion.jpeg') }}" alt="Bouteille au marché" class="welcome-img">
-    <div class="welcome-informations">
-        <form action="/register" class="welcome-form" method="post" id="registration">
-            @csrf
-            <div class="welcome-input-container">
-                <label for="nom" class="welcome-form-label">NOM</label>
-                <input type="text" id="nom" name="nom" class="welcome-form-input">
-            </div>
-            <div class="welcome-input-container">
-                <label for="prenom" class="welcome-form-label">PRÉNOM</label>
-                <input type="text" id="prenom" name="prenom" class="welcome-form-input">
-            </div>
-            <div class="welcome-input-container">
-                <label for="email" class="welcome-form-label">EMAIL</label>
-                <input type="text" id="email" name="email" class="welcome-form-input">
-            </div>
-            <div class="welcome-input-container">
-                <label for="password" class="welcome-form-label">PASSWORD</label>
-                <input type="password" id="password" name="password" class="welcome-form-input">
-            </div>
-        </form>
-        <div class="welcome-forgot-psw">
-            <a href="#">MOT DE PASSE OUBLIÉ</a>
-        </div>
-        <button class="welcome-connection-btn" type="submit" form="registration">CRÉER UN COMPTE</button>
-        <a href="{{ route('login') }}">SE CONNECTER</a>
-    </div>
-</div> -->
 <header>
     vino
 </header>
