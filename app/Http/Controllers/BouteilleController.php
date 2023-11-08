5a0bab7f9a4d2ce93ac93900907fdbfdb8f5978b
frizi10 <ismail.frizi@gmail.com>
2023-11-08 10:42:01 -0500

    ajout BouteilleController

--- app/Http/Controllers/BouteilleController.php
+++ app/Http/Controllers/BouteilleController.php
@@ -12,45 +12,68 @@ class BouteilleController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        $bouteilles = Bouteille::paginate(7);
-       
-        return view('bouteille.index', ['bouteilles'=> $bouteilles]);
-    }
+     public function index()
+     {
+         $bouteilles = Bouteille::paginate(7);
+ 
+         return view('bouteille.index', ['bouteilles'=> $bouteilles]);
+     }
 
 
-    public function search(Request $request) {
+     public function search(Request $request) { 
+         $searchTerm = $request->input('search');
+         $bouteilles = Bouteille::where('nom', 'like', "%$searchTerm%")->paginate(3);
+         
+       return view('bouteille.show-search', ['bouteilles' => $bouteilles]);
+     }
 
-        $searchTerm = $request->input('search');
-        $bouteilles = Bouteille::where('nom', 'like', "%$searchTerm%")->paginate(3);
-               
-      return view('bouteille.show-search', ['bouteilles' => $bouteilles]);
-    }
+     public function sorting (Request $request) { 
+         $sort = $request->input('sort');
+         switch ($sort) {
+             case 'price-asc':
+                 $bouteilles = Bouteille::orderBy('prix', 'asc')->paginate(3);
+                 break;
+             case 'price-desc':
+                 $bouteilles = Bouteille::orderBy('prix', 'desc')->paginate(3);
+                 break;
+             case 'name-asc':
+                 $bouteilles = Bouteille::orderBy('nom', 'asc')->paginate(3);
+                 break;
+             case 'name-desc':
+                 $bouteilles = Bouteille::orderBy('nom', 'desc')->paginate(3);
+                 break;
+             default:
+                 $bouteilles = Bouteille::all();
+                 break;
+         }
+         return view('bouteille.show-sorting', compact('bouteilles'));
+  
+     }
 
-    public function sorting (Request $request) {
-
-        $sort = $request->input('sort');
-        switch ($sort) {
-            case 'price-asc':
-                $bouteilles = Bouteille::orderBy('prix', 'asc')->paginate(3);
-                break;
-            case 'price-desc':
-                $bouteilles = Bouteille::orderBy('prix', 'desc')->paginate(3);
-                break;
-            case 'name-asc':
-                $bouteilles = Bouteille::orderBy('nom', 'asc')->paginate(3);
-                break;
-            case 'name-desc':
-                $bouteilles = Bouteille::orderBy('nom', 'desc')->paginate(3);
-                break;
-            default:
-                $bouteilles = Bouteille::all();
-                break;
-        }
-        return view('bouteille.show-sorting', compact('bouteilles'));
-        
-    }
+     public function filtrerProduits(Request $request)
+     {
+         // Récupérez les valeurs des filtres 
+         $filtres = $request->only(['couleur', 'format', 'pays', 'millesime', 'region', 'prix_min', 'prix_max']);
+         // Initialisez la variable $fbouteilles en tant que requête de modèle
+         $fbouteilles = Bouteille::query();
+         foreach ($filtres as $champ => $valeur) {
+             if ($valeur !== null) 
+                 if ($champ === 'prix_min') {
+                     $fbouteilles->where('prix', '>=', $valeur);
+                 } elseif ($champ === 'prix_max') {
+                     $fbouteilles->where('prix', '<=', $valeur);
+                 } else {
+              
+                     $fbouteilles->where($champ, $valeur);
+                 }
+             }
+         
+         // Paginer les résultats
+         $fbouteilles = $fbouteilles->paginate(5);
+         // Retournez la vue avec les résultats filtrés
+         return view('bouteille.show-filter-results', ['fbouteilles' => $fbouteilles]);
+     }
+    
 
     /**
      * Show the form for creating a new resource.
@@ -118,4 +141,33 @@ class BouteilleController extends Controller
     {
         //
     }
+    // public function index(Request $request)
+    // {
+    //     $bouteillesQuery = Bouteille::query();
+    //     $nom = ($request->input('search'));
+    //     $prix_min = ($request->input('prix_min'));
+    //     $prix_max = ($request->input('prix_max'));
+       
+
+    //     if(!empty($nom)) {
+    //         $bouteillesQuery->where('nom', 'like',"%{$nom}%");
+    //     }
+
+    //     // if(!empty($prix_min)) {
+    //     //     $bouteillesQuery->where('prix', '>=',"%{$prix_min}%");
+    //     // }
+
+    //     // if(!empty($prix_max)) {
+    //     //     $bouteillesQuery->where('prix', '<>=',"%{$prix_max}%");
+    //     // }
+        
+        
+    //     //dd($nom);
+        
+    //     $bouteilles = $bouteillesQuery->paginate(10);
+        
+    //     return view('bouteille.index', ['bouteilles'=> $bouteilles]);
+
+        
+    // }
 }
