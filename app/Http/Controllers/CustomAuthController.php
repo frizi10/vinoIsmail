2e2bb66333d3d28179e65ae4241a23c223a23474
ElodieBou44 <elodieboulanger4@outlook.com>
2023-11-08 13:15:12 -0500

    Gestion des erreurs - Authentification

--- app/Http/Controllers/CustomAuthController.php
+++ app/Http/Controllers/CustomAuthController.php
@@ -38,65 +38,64 @@ class CustomAuthController extends Controller
      */
     public function store(Request $request)
     {
-        //  dd($request->all());
         $request->validate([
-            'nom' => 'required|min:2|max:20|alpha',
-            'email' => 'required|email',
+            'nom'      => 'required|min:2|max:20|alpha',
+            'email'    => 'required|email',
             'password' => 'required|min:6|confirmed'
         ],
         [
-            'nom.required' => 'Veuillez saisir votre nom',
-            'nom.min' => 'Votre nom doit contenir au moins 2 caractères',
-            'nom.max' => 'Votre nom ne doit pas dépasser 20 caractères',
-            'nom.alpha' => 'Votre nom ne doit contenir que des lettres',
-            'email.required' => 'Veuillez saisir votre adresse email',
+            'nom.required'      => 'Veuillez saisir votre nom',
+            'nom.min'           => 'Votre nom doit contenir au moins 2 caractères',
+            'nom.max'           => 'Votre nom ne doit pas dépasser 20 caractères',
+            'nom.alpha'         => 'Votre nom ne doit contenir que des lettres',
+            'email.required'    => 'Veuillez saisir votre adresse email',
             'password.required' => 'Veuillez saisir votre mot de passe',
-            'password.min' => 'Votre mot de passe doit contenir au moins 6 caractères',
-            'password.confirmed' => 'Les mots de passe ne correspondent pas'
-        ]
-    );
+            'password.min'      => 'Votre mot de passe doit contenir au moins 6 caractères',
+            'password.confirmed'=> 'Les mots de passe ne correspondent pas'
+        ]);
 
         $user = new User;
         $user->nom = $request->input('nom');
         $user->email = $request->input('email');
         $user->password = Hash::make($request->input('password'));
         $user->save();
-        return redirect(route('welcome'))->withSuccess('Utilisateur enregistré');
+        return redirect(route('welcome'))->withSuccess('Compte créé avec succès, vous pouvez maintenant vous connecter.');
     }
 
     public function authentication(Request $request)
     {
         $request->validate([
-            'email' => 'required|email|exists:users',
+            'email'    => 'required|email|exists:users',
             'password' => 'required'
         ], [
-            
-            'email.required' => 'Veuillez saisir votre adresse email',
-            'email.email' => 'Veuillez entrer une adresse email valide',
-            'password.required' => 'Veuillez saisir votre mot de passe',
-            ]
-        );
-    
-        $credentials = $request->only('email', 'password');
-        // dd($credentials);
-    // dd(Auth::validate($credentials)
-    // );
-        if (!Auth::validate($credentials)) {
+            'email.required'     => "Veuillez saisir votre adresse email",
+            'email.email'        => "Veuillez entrer une adresse email valide",
+            'email.exists' => "Ce courriel n'est pas associé à un compte",
+            'password.required'  => "Veuillez saisir votre mot de passe",
+        ]);
+        try {
+
+            $credentials = $request->only('email', 'password');
+
+            if (!Auth::validate($credentials)) {
+                return redirect('login')
+                    ->withErrors([
+                        'erreur' => "L'adresse email ou le mot de passe est incorrect"
+                    ]);
+            }
+
+            $user = Auth::getProvider()->retrieveByCredentials($credentials);
+
+            Auth::login($user, $request->get('remember'));
+
+            return redirect()->route('welcome'); 
+        } catch (\Exception $e) {
             return redirect('login')
                 ->withErrors([
-                    'email' => 'ladresse email ou le mot de passe est incorrect'
-                ])
-                ->withInput();
+                    'erreur' => "Une erreur s'est produite lors de l'authentification"
+                ]);
         }
-    
-        $user = Auth::getProvider()->retrieveByCredentials($credentials);
-    
-        Auth::login($user, $request->get('remember'));
-    
-        return redirect()->route('welcome'); 
-        // return redirect()->route('welcome')->with('success', 'Vous êtes connectés')->with('name', $user->nom);
     }
-    
 
     public function logout(){
         Auth::logout();
@@ -123,7 +122,7 @@ class CustomAuthController extends Controller
      */
     public function edit(User $user)
     {
-        //
+        return view('utilisateur.edit', ['user' => $user]);
     }
 
     /**
@@ -135,7 +134,33 @@ class CustomAuthController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        try {
+            $request->validate([
+                'nom'   => 'required|min:2|max:20|alpha',
+                'email' => 'required|email',
+            ],
+            [
+                'nom.required'   => 'Veuillez saisir votre nom',
+                'nom.min'        => 'Votre nom doit contenir au moins 2 caractères',
+                'nom.max'        => 'Votre nom ne doit pas dépasser 20 caractères',
+                'nom.alpha'      => 'Votre nom ne doit contenir que des lettres',
+                'email.required' => 'Veuillez saisir votre adresse courriel', 
+                'email.email'    => 'Veuillez saisir un courriel valide'
+            ]); 
+
+            // if ($request->fails()) {
+            //     return view('profil.edit', $user->id, ['user' => $user, 'errors' => $request->errors()]);
+            // }
+    
+            $user->update([
+                'nom' => $request->nom,
+                'email' => $request->email
+            ]);
+    
+            return redirect(route('profil.show', $user->id))->withSuccess('Profil mis à jour avec succès');
+        } catch (\Exception $e) {
+            return redirect(route('profil.edit', $user->id))->withErrors(["Une erreur s'est produite lors de la mise à jour du profil"]);
+        }
     }
 
     /**
