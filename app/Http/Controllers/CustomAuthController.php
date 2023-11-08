fb1db96d6f02e7a0b1df420a4e9c7933ece002d4
ElodieBou44 <elodieboulanger4@outlook.com>
2023-11-08 16:45:54 -0500

    Ajout de signatures de fonctions

--- app/Http/Controllers/CustomAuthController.php
+++ app/Http/Controllers/CustomAuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Models\Role;
 
 class CustomAuthController extends Controller
 {
