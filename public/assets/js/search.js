a74e4c790efba97b7cb010940bd056508e16bd49
frizi10 <ismail.frizi@gmail.com>
2023-11-09 16:36:33 -0500

    effacer search.js

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,7 +1,12 @@
-var searchInput = document.getElementById('search');
 
-// Ajoutez un écouteur d'événements pour le changement d'entrée dans le champ de recherche
-searchInput.addEventListener('input', function () {
-    // Soumettez automatiquement le formulaire lorsqu'il y a un changement dans le champ de recherche
-    document.getElementById('searchForm').submit();
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     console.log("Script chargé");
+
+//     document.getElementById('searchForm').addEventListener('submit', function (event) {
+      
+      
+//         document.getElementById('searchForm').submit();
+//     });
+// });
+
+
