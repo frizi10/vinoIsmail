f8a2c36296ce665784e721db3bfce6037dbd19cd
ElodieBou44 <elodieboulanger4@outlook.com>
2023-11-09 22:00:40 -0500

    Ajout de la classe active de la nav selon la page sur laquelle on se trouve

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
         <ul class="nav-admin-list">
             <li class="main-nav-item">        
                 <a href="{{ route('admin.index') }}">
-                    <figure class="container-icons-navbar active">
+                    <figure class="container-icons-navbar @if(Route::currentRouteName() == 'admin.index') active @endif">
                         <img src="{{ asset('assets/icons/admin_users_icon.svg') }}" alt="Accueil">
                         <figcaption>utilisateurs</figcaption>
                     </figure>
@@ -28,7 +28,7 @@
             </li>
             <li class="main-nav-item">        
                 <a href="{{ route('bouteille.index') }}">
-                    <figure class="container-icons-navbar">
+                    <figure class="container-icons-navbar @if(Route::currentRouteName() == 'bouteille.index') active @endif">
                         <img src="{{ asset('assets/icons/add_icon.svg') }}" alt="Recherche">
                         <figcaption class="icons-label">bouteilles</figcaption>
                     </figure>
@@ -44,7 +44,7 @@
             </li>
             <li class="main-nav-item">         
                 <a href="{{ route('profil.show', Auth::user()->id) }}">
-                    <figure class="container-icons-navbar">
+                    <figure class="container-icons-navbar @if(Route::currentRouteName() == 'profil.show') active @endif">
                         <img src="{{ asset('assets/icons/profile_icon.svg') }}" alt="Profil">
                         <figcaption>profil</figcaption>
                     </figure>
@@ -58,7 +58,7 @@
         <ul class="main-nav-list">
             <li class="main-nav-item">        
                 <a href="{{ route('welcome') }}">
-                    <figure class="container-icons-navbar active">
+                    <figure class="container-icons-navbar @if(Route::currentRouteName() == 'welcome') active @endif">
                         <img src="{{ asset('assets/icons/home_icon.svg') }}" alt="Accueil">
                         <figcaption>accueil</figcaption>
                     </figure>
@@ -66,7 +66,7 @@
             </li>
             <li class="main-nav-item">        
                 <a href="{{ route('bouteille.index') }}">
-                    <figure class="container-icons-navbar">
+                    <figure class="container-icons-navbar @if(Route::currentRouteName() == 'bouteille.index') active @endif">
                         <img src="{{ asset('assets/icons/add_icon.svg') }}" alt="Recherche">
                         <figcaption class="icons-label">ajouter</figcaption>
                     </figure>
@@ -74,7 +74,7 @@
             </li>
             <li class="main-nav-item">
                 <a href="{{ route('liste.index') }}">
-                    <figure class="container-icons-navbar">
+                    <figure class="container-icons-navbar @if(Route::currentRouteName() == 'liste.index') active @endif">
                         <img src="{{ asset('assets/icons/list_icon.svg') }}" alt="Liste d'achats">
                         <figcaption>liste</figcaption>
                     </figure>
@@ -82,7 +82,7 @@
             </li>
             <li class="main-nav-item">        
                 <a href="{{ route('cellier.index') }}">
-                    <figure class="container-icons-navbar">
+                    <figure class="container-icons-navbar @if(Route::currentRouteName() == 'cellier.index') active @endif">
                         <img src="{{ asset('assets/icons/cellars_icon.svg') }}" alt="Celliers">
                         <figcaption>celliers</figcaption>
                     </figure>
@@ -90,7 +90,7 @@
             </li>
             <li class="main-nav-item">         
                 <a href="{{ route('profil.show', Auth::user()->id) }}">
-                    <figure class="container-icons-navbar">
+                    <figure class="container-icons-navbar @if(Route::currentRouteName() == 'profil.show') active @endif">
                         <img src="{{ asset('assets/icons/profile_icon.svg') }}" alt="Profil">
                         <figcaption>profil</figcaption>
                     </figure>
